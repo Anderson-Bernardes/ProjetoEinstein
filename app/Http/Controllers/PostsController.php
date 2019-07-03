ec1378f3b17d34135871a43d03830d998826b8b3
Anderson Parca Bernardes <anderson_megax@hotmail.com>
2019-07-03 20:16:26 -0300

    Criando controller para postagens e metodo para gravação de posts

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        date_default_timezone_set('America/Sao_Paulo');
+        $user = Auth::user();
+        //echo 'to dentro do echo da pagina de posts';
+        //echo $request['grupo'];
+        //echo $request['texto'];
+        if(isset($request['imagem'])){
+            $imagem = $request['imagem'];
+        }
+        else
+            $imagem = null;
+
+        $dados =['user_id'  => $user->getAuthIdentifier(),
+                'grupo_id'  => $request['grupo'],
+                'texto'     => $request['texto'],
+                'imagem'    => $imagem,
+                'created_at'=> date('Y-m-d H:i:s'),
+                'updated_at'=> date('Y-m-d H:i:s')];
+
+
+        print_r($dados);
+        //$this->create()
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
