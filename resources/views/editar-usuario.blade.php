77cf82c92eaebef471fe088cea0450c0f6c7ff26
Anderson Parca Bernardes <anderson_megax@hotmail.com>
2019-08-31 20:36:23 -0300

    Adiçoes de funcoes na tela de configuracao de usuario

--- resources/views/editar-usuario.blade.php
+++ resources/views/editar-usuario.blade.php
@@ -74,7 +74,7 @@
                             </i>
 
                         </a>
-                        <a class="nav-link" href="#">
+                        <a class="nav-link" href="{{route('userPerfil')}}">
                             {{--<img
                                 src="https://scontent.fpoo3-1.fna.fbcdn.net/v/t1.0-9/14517422_945412785570713_2191894243524876629_n.jpg?_nc_cat=109&_nc_ht=scontent.fpoo3-1.fna&oh=9a20577dddaca6b6a1507978493fbbe2&oe=5D70C21D"
                                 id="image" alt="">--}}
@@ -126,11 +126,13 @@
                     </div>
                 </form>
             </div>
-            <div class="card cardinfo">
+            <form action="{{route('updateDadosUser')}}" method="post">
+                {{csrf_field()}}
+                <div class="card cardinfo">
                 <div class="info">
                     <p class="mt-2" style="text-align: center;">Editar perfil</p>
-                    <input class="form-control form-control mt-2" type="text" placeholder="Nome" value="{{$user->name}}">
-                    <input class="form-control form-control mt-2" type="text" placeholder="@Apelido" value="{{$user->username}}">
+                    <input name="nome" class="form-control form-control mt-2" type="text" placeholder="Nome" value="{{$user->name}}">
+                    <input name="user" class="form-control form-control mt-2" type="text" placeholder="@Apelido" value="{{$user->username}}">
                    {{-- <select class="custom-select mt-2" id="inlineFormCustomSelectPref">
                         <option selected value="1">Estudante</option>
                         <option value="2">Professor</option>
@@ -161,38 +163,6 @@
                                             @for($i=1; $i<=31; $i++)
                                                 <option value="{{$i}}">{{$i}}</option>
                                             @endfor
-                                            {{--<option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                            <option value="6">6</option>
-                                            <option value="7">7</option>
-                                            <option value="8">8</option>
-                                            <option value="9">9</option>
-                                            <option value="10">10</option>
-                                            <option value="11">11</option>
-                                            <option value="12">12</option>
-                                            <option value="13">13</option>
-                                            <option value="14">14</option>
-                                            <option value="15">15</option>
-                                            <option value="16">16</option>
-                                            <option value="17">17</option>
-                                            <option value="18">18</option>
-                                            <option value="19">19</option>
-                                            <option value="20">20</option>
-                                            <option value="21">21</option>
-                                            <option value="22">22</option>
-                                            <option value="23">23</option>
-                                            <option value="24">24</option>
-                                            <option value="25">25</option>
-                                            <option value="26">26</option>
-                                            <option value="27">27</option>
-                                            <option value="28">28</option>
-                                            <option value="29">29</option>
-                                            <option value="30">30</option>
-                                            <option value="31">31</option>--}}
-
                                         </select>
                                     </div>
                                     <div class="col-6">
@@ -201,125 +171,6 @@
                                             @for($j=2019; $j>=1970; $j--)
                                                 <option value="{{$j}}">{{$j}}</option>
                                             @endfor
-                                            {{--<option value="2019">2019</option>
-                                            <option value="2018">2018</option>
-                                            <option value="2017">2017</option>
-                                            <option value="2016">2016</option>
-                                            <option value="2015">2015</option>
-                                            <option value="2014">2014</option>
-                                            <option value="2013">2013</option>
-                                            <option value="2012">2012</option>
-                                            <option value="2011">2011</option>
-                                            <option value="2010">2010</option>
-                                            <option value="2009">2009</option>
-                                            <option value="2008">2008</option>
-                                            <option value="2007">2007</option>
-                                            <option value="2006">2006</option>
-                                            <option value="2005">2005</option>
-                                            <option value="2004">2004</option>
-                                            <option value="2003">2003</option>
-                                            <option value="2002">2002</option>
-                                            <option value="2001">2001</option>
-                                            <option value="2000">2000</option>
-                                            <option value="1999">1999</option>
-                                            <option value="1998">1998</option>
-                                            <option value="1997">1997</option>
-                                            <option value="1996">1996</option>
-                                            <option value="1995">1995</option>
-                                            <option value="1994">1994</option>
-                                            <option value="1993">1993</option>
-                                            <option value="1992">1992</option>
-                                            <option value="1991">1991</option>
-                                            <option value="1990">1990</option>
-                                            <option value="1989">1989</option>
-                                            <option value="1988">1988</option>
-                                            <option value="1987">1987</option>
-                                            <option value="1986">1986</option>
-                                            <option value="1985">1985</option>
-                                            <option value="1984">1984</option>
-                                            <option value="1983">1983</option>
-                                            <option value="1982">1982</option>
-                                            <option value="1981">1981</option>
-                                            <option value="1980">1980</option>
-                                            <option value="1979">1979</option>
-                                            <option value="1978">1978</option>
-                                            <option value="1977">1977</option>
-                                            <option value="1976">1976</option>
-                                            <option value="1975">1975</option>
-                                            <option value="1974">1974</option>
-                                            <option value="1973">1973</option>
-                                            <option value="1972">1972</option>
-                                            <option value="1971">1971</option>
-                                            <option value="1970">1970</option>
-                                            <option value="1969">1969</option>
-                                            <option value="1968">1968</option>
-                                            <option value="1967">1967</option>
-                                            <option value="1966">1966</option>
-                                            <option value="1965">1965</option>
-                                            <option value="1964">1964</option>
-                                            <option value="1963">1963</option>
-                                            <option value="1962">1962</option>
-                                            <option value="1961">1961</option>
-                                            <option value="1960">1960</option>
-                                            <option value="1959">1959</option>
-                                            <option value="1958">1958</option>
-                                            <option value="1957">1957</option>
-                                            <option value="1956">1956</option>
-                                            <option value="1955">1955</option>
-                                            <option value="1954">1954</option>
-                                            <option value="1953">1953</option>
-                                            <option value="1952">1952</option>
-                                            <option value="1951">1951</option>
-                                            <option value="1950">1950</option>
-                                            <option value="1949">1949</option>
-                                            <option value="1948">1948</option>
-                                            <option value="1947">1947</option>
-                                            <option value="1946">1946</option>
-                                            <option value="1945">1945</option>
-                                            <option value="1944">1944</option>
-                                            <option value="1943">1943</option>
-                                            <option value="1942">1942</option>
-                                            <option value="1941">1941</option>
-                                            <option value="1940">1940</option>
-                                            <option value="1939">1939</option>
-                                            <option value="1938">1938</option>
-                                            <option value="1937">1937</option>
-                                            <option value="1936">1936</option>
-                                            <option value="1935">1935</option>
-                                            <option value="1934">1934</option>
-                                            <option value="1933">1933</option>
-                                            <option value="1932">1932</option>
-                                            <option value="1931">1931</option>
-                                            <option value="1930">1930</option>
-                                            <option value="1929">1929</option>
-                                            <option value="1928">1928</option>
-                                            <option value="1927">1927</option>
-                                            <option value="1926">1926</option>
-                                            <option value="1925">1925</option>
-                                            <option value="1924">1924</option>
-                                            <option value="1923">1923</option>
-                                            <option value="1922">1922</option>
-                                            <option value="1921">1921</option>
-                                            <option value="1920">1920</option>
-                                            <option value="1919">1919</option>
-                                            <option value="1918">1918</option>
-                                            <option value="1917">1917</option>
-                                            <option value="1916">1916</option>
-                                            <option value="1915">1915</option>
-                                            <option value="1914">1914</option>
-                                            <option value="1913">1913</option>
-                                            <option value="1912">1912</option>
-                                            <option value="1911">1911</option>
-                                            <option value="1910">1910</option>
-                                            <option value="1909">1909</option>
-                                            <option value="1908">1908</option>
-                                            <option value="1907">1907</option>
-                                            <option value="1906">1906</option>
-                                            <option value="1905">1905</option>
-                                            <option value="1904">1904</option>
-                                            <option value="1903">1903</option>
-                                            <option value="1902">1902</option>
-                                            <option value="1901">1901</option>--}}
                                         </select>
                                     </div>
                                 </div>
@@ -344,7 +195,7 @@
                         </div>
                     </div>
                     <div class="botao">
-                        <button type="button" class="btn btn-default mt-4">Salvar</button>
+                        <button type="submit" class="btn btn-default mt-4">Salvar</button>
                     </div>
 
                 </div>
@@ -352,32 +203,35 @@
 
             </div>
 
-
+            </form>
         </div>
 
 
         <div class="col-9">
             <div class="card editarperfil">
-                <strong class="mt-3"><p>Dados pessoais</p></strong>
+                <form method="POST" action="{{route('updateDadosUser') }}">
+                    {{csrf_field()}}
+
+                    <strong class="mt-3"><p>Dados pessoais</p></strong>
                 <form>
                     <fieldset disabled>
                         <div class="form-group">
                             <label for="disabledTextInput">E-mail:</label>
                             <input type="text" id="disabledTextInput" class="form-control"
-                                   placeholder="Celysastre@hotmail.com" value="{{$user->email}}">
+                                    value="{{$user->email}}">
                         </div>
                     </fieldset>
                     <div class="form-group">
                         <label for="formGroupExampleInput">Sexo:</label>
-                        <select class="custom-select" id="sexo">
+                        <select class="custom-select" id="sexo" name="sexo">
                             <!--<option value="nd"></option>-->
                             <option value="NE" selected>Não identificar</option>
-                            <option value="fem"
+                            <option value="F"
                             @if($user->sexo=='F')
                                 selected
                                 @endif
                             >Feminino</option>
-                            <option value="masc"
+                            <option value="M"
                             @if($user->sexo == 'M')
                                 selected
                                 @endif
@@ -387,7 +241,7 @@
                     </div>
                     <div class="form-group">
                         <label for="formGroupExampleInput">Estado:</label>
-                        <select class="custom-select" id="uf">
+                        <select class="custom-select" id="uf" name="uf">
                             <option value="nd"></option>
                             <option value="AC">Acre</option>
                             <option value="AL">Alagoas</option>
@@ -430,30 +284,34 @@
                     </div>
                 </form>
                 <div class="botao">
-                    <button type="submit" class="btn btn-default mt-4 mb-4">Salvar</button>
+                    <button type="submit"  class="btn btn-default mt-4 mb-4">Salvar</button>
                 </div>
-
+                </form>
 
             </div>
             <div class="card excluirperfil">
-                <strong class="mt-3"><p>Conta</p></strong>
+                    <strong class="mt-3"><p>Conta</p></strong>
                 <div class="form-group">
+
                     <label for="inputPassword5">Nova senha:</label>
-                    <input type="password" id="inputPassword5" class="form-control"
+                    <input type="password" id="inputPassword5" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required
                            aria-describedby="passwordHelpBlock">
+
+
                     <small id="passwordHelpBlock" class="form-text text-muted">
                         sua senha tem que possuir de 8-12 caracteres, contendo letras ou números, sem espaço ou
                         caracteres especiais.
                     </small>
                     <label for="inputPassword5" class="mt-2">Digite a senha novamente:</label>
                     <input type="password" id="inputPassword5" class="form-control"
-                           aria-describedby="passwordHelpBlock">
+                           aria-describedby="passwordHelpBlock" name="password_confirmation" required>
                     <small id="passwordHelpBlock" class="form-text text-muted"></small>
                     <hr>
                     <a href="" class="mt-4" style="text-align: center;"><p>Excluir conta</p></a>
+
                 </div>
                 <div class="botao">
-                    <button type="button" class="btn btn-default mt-4 mb-4">Salvar</button>
+                    <button type="submit" class="btn btn-default mt-4 mb-4">Salvar</button>
                 </div>
             </div>
         </div>
